f024a5f4c232a6d8c6b9b81a0f58a2e9249ecf25
Rafael Alves <rafaelpereiraalves@ufba.br>
2024-03-18 10:55:32 -0300

    Construct Category

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,4 +1,13 @@
 
+from datetime import datetime
+
 class Category:
-    def __init__(self,name) -> None:
+    def __init__(self, name: str, description: str , is_active: bool, created_ad: datetime) -> None:
         self.name = name
+        self.description = description
+        self.is_active = is_active
+        self.created_ad = created_ad
+
+
+
+# Create Caterory is a Enterprise Business Rule, not is datebase. Database is persistence.
